3b5d74896045759893cee5ba734f6f76cfa052be
vinentHuynh <vincenthuynh02@gmail.com>
2022-09-02 14:52:33 -0500

    Initial Commit

---
+++ AssetManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AssetManager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ AssetManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AssetManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
