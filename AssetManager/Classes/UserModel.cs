dae449153ebfb9443d722c63b95fd556c23a80ee
chuynh017 <christine.huynh017@gmail.com>
2022-09-30 12:40:53 -0500

    added login implementation

---
+++ AssetManager/Classes/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssetManager.Classes
+{
+    public class UserModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool LoginValidate(out string token)
+        {
+            if (Name.Equals("admin") && Password.Equals("password"))
+            {
+                token = "1";
+                return true;
+            }
+            else
+            {
+                token = null;
+                return false;
+            }
+
+        }
+
+    }
+}
